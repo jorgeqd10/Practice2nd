e230726ba184d710a50ca4a1ceada6bd4c5e560e
jorgeqd10 <77657075+jorgeqd10@users.noreply.github.com>
2024-04-16 20:08:14 -0600

    LoginRefactor
    Refactor login

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,26 +1,28 @@
 package steps;
 
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import po.LoginPage;
+
+import java.util.List;
 
 public class LoginSteps {
 
     LoginPage loginPage = new LoginPage();
 
-    @Then("I enter {string} username")
-    public void iEnterUsername(String username) {
-        loginPage.enterUsername(username);
-    }
-
-    @Then("I enter {string} password")
-    public void iEnterPassword(String password) {
+    @When("User enters valid credentials")
+    public void userEntersValidCredentials(DataTable dataTable) throws InterruptedException{
+        System.out.println("Credentials Entered");
+        List<List<String>> signUpForm = dataTable.asLists(String.class);
+        String userName = signUpForm.get(0).get(0);
+        String password = signUpForm.get(0).get(1);
+        loginPage.enterUsername(userName);
         loginPage.enterPassword(password);
-    }
-
-    @And("I click login button")
-    public void iClickLoginButton() {
         loginPage.clickLoginBtn();
     }
+
+
 }
